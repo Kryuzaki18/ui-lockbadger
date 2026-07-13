aaaeb3b20c5bff580562f881187959a4525f77af
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-07-13 23:54:56 +0800

    chore: separate guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { AuthComponent } from './features/auth/auth.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
 import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
-import { authGuard, guestGuard } from './core/guards/auth.guard';
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
 import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
--- src/app/core/guards/guest.guard.ts
+++ src/app/core/guards/guest.guard.ts
@@ -6,14 +6,14 @@ import { filter, map, of, switchMap, take } from 'rxjs';
 import * as AuthActions from '../../store/auth/auth.actions';
 import { selectAuthState } from '../../store/auth/auth.selectors';
 
-export const authGuard: CanActivateFn = () => {
+export const guestGuard: CanActivateFn = () => {
   const store = inject(Store);
   const router = inject(Router);
 
   return store.select(selectAuthState).pipe(
     take(1),
     switchMap((state) => {
-      if (state.user) return of(true);
+      if (state.user) return of(router.createUrlTree(['/home']));
 
       if (!state.isLoading) {
         store.dispatch(AuthActions.checkAuth());
@@ -22,7 +22,7 @@ export const authGuard: CanActivateFn = () => {
       return store.select(selectAuthState).pipe(
         filter((s) => !s.isLoading),
         take(1),
-        map((s) => (s.user ? true : router.createUrlTree(['/login']))),
+        map((s) => (s.user ? router.createUrlTree(['/home']) : true)),
       );
     }),
   );
