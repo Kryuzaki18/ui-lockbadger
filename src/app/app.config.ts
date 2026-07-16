e38120b793e6a9420f37ab971095d2566cfd5372
Krystian John Dumapit <kjedumapit@gmail.com>
2026-07-17 00:44:31 +0800

    chore: update file structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, TitleStrategy } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
 
 import { routes } from './app.routes';
-import { AppTitleStrategy } from './strategy/app-title.strategy';
+import { AppTitleStrategy } from './core/strategy/app-title.strategy';
 
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import * as authEffects from './store/auth/auth.effects';
 import { AUTH_FEATURE_KEY, authReducer } from './store/auth/auth.reducer';
+
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
 
 registerLocaleData(en);
 
